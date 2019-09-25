d42ac6aa31ca9c5060600b0509418c8974a951f3
David Gouge <davegouge@googlemail.com>
2019-09-25 11:33:21 +0100

    Logging

--- src/SFA.DAS.Assessor.Functions/Startup.cs
+++ src/SFA.DAS.Assessor.Functions/Startup.cs
@@ -1,9 +1,8 @@
-using System.IO;
-using System.Reflection;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using NLog;
-using NLog.Config;
 using NLog.Extensions.Logging;
 
 [assembly: FunctionsStartup(typeof(SFA.DAS.Assessor.Functions.WorkflowMigrator.Startup))]
@@ -12,42 +11,14 @@ namespace SFA.DAS.Assessor.Functions.WorkflowMigrator
 {
     public class Startup : FunctionsStartup
     {
-        public Startup()
-        {
-            var fileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
-            string path = fileInfo.Directory.Parent.FullName;
-            LogManager.Configuration = new XmlLoggingConfiguration($@"{path}\nlog.config");
-        }
-
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddLogging((loggingBuilder) => {
-                loggingBuilder.AddNLog();
+            builder.Services.AddLogging((options) => {
+                options.SetMinimumLevel(LogLevel.Trace);
+                //options.AddNLog(new NLogProviderOptions{});
+                options.AddConsole();
+                options.AddDebug();
             });
-
-            // var sp = builder.Services.BuildServiceProvider();
-
-            // var logger = sp.GetService<Microsoft.Extensions.Logging.ILogger>();
-            // logger.LogInformation("WORKFLOWMIGRATE - GOT LOGGER");
-
-            // var configuration = sp.GetService<IConfiguration>();
-
-            // var config = new ConfigurationBuilder()
-            //     .AddConfiguration(configuration)
-            //     .AddAzureTableStorageConfiguration(
-            //         System.Environment.GetEnvironmentVariable("ConfigurationStorageConnectionString", EnvironmentVariableTarget.Process),
-            //         "SFA.DAS.Assessor.Functions",
-            //         System.Environment.GetEnvironmentVariable("EnvironmentName", EnvironmentVariableTarget.Process),
-            //         "1.0"
-            //     ).Build();
-            
-            // logger.LogInformation("WORKFLOWMIGRATE - Built config");
-
-            //builder.Services.AddOptions().Configure<SqlConnectionStrings>(config.GetSection("SqlConnectionStrings"));
-
-            
         }
     }
-
-    
 }
