ad3d11a34e71769450d93874cf26acd6eeac1f61
Dave Gouge <davegouge@gmail.com>
2019-10-16 15:29:06 +0100

    More migration

--- src/SFA.DAS.Assessor.Functions/ApplicationsMigrator.cs
+++ src/SFA.DAS.Assessor.Functions/ApplicationsMigrator.cs
@@ -31,20 +31,48 @@ namespace SFA.DAS.Assessor.Functions
             using (var qnaConnection = new SqlConnection(_connectionStrings.QnA))
             using (var assessorConnection = new SqlConnection(_connectionStrings.Assessor))
             {
-                var applications = applyConnection.Query("SELECT * FROM Applications WHERE ApplicationStatus NOT IN ('Approved','Rejected')");
-                foreach (var appl in applications)
+                var workflowId = qnaConnection.QuerySingleOrDefault<Guid?>("SELECT Id FROM Workflows WHERE Type = 'EPAO'");
+                if (workflowId is null)
                 {
-                    string id = appl.Id.ToString();
-                    log.LogInformation(id);
+                    throw new ApplicationException("Workflow of Type 'EPAO' not found.");
+                }
+
+                var applyApplications = applyConnection.Query("SELECT * FROM Applications WHERE ApplicationStatus NOT IN ('Approved','Rejected')");
+                foreach (var originalApplyApplication in applyApplications)
+                {
+                    var qnaApplicationId = Guid.NewGuid();
+
+                    // Create Qna Applications record
+                    qnaConnection.Execute(@"INSERT INTO Applications (Id, WorkflowId, Reference, CreatedAt, ApplicationStatus, ApplicationData) 
+                                            VALUES (@Id, @WorkflowId, 'Migrated from Apply', @CreatedAt, @ApplicationStatus, '')",
+                                            new {Id = qnaApplicationId, WorkflowId = workflowId, originalApplyApplication.CreatedAt, originalApplyApplication.ApplicationStatus});
+
+                    var applySequences =  applyConnection.Query("SELECT * FROM ApplicationSequences WHERE ApplicationId = @ApplicationId", new {ApplicationId = originalApplyApplication.Id});
+
+                    foreach (var applySequence in applySequences) 
+                    {
+                        qnaConnection.Execute("INSERT INTO ApplicationSequences (Id, ApplicationId, SequenceNo, IsActive) VALUES (@Id, @ApplicationId, @SequenceNo, @IsActive)",
+                                                new {Id = applySequence.Id, ApplicationId = qnaApplicationId, SequenceNo = applySequence.SequenceId, IsActive = applySequence.IsActive});
+
+                        var applySections =  applyConnection.Query("SELECT * FROM ApplicationSections WHERE ApplicationId = @ApplicationId AND SequenceId = @SequenceId", 
+                                                                    new {ApplicationId = originalApplyApplication.Id, SequenceId = applySequence.SequenceId});
+                        foreach (var applySection in applySections)
+                        {
+                            qnaConnection.Execute(@"INSERT INTO ApplicationSections (Id, ApplicationId, SequenceNo, SectionNo, QnaData, Title, LinkTitle, DisplayType, SequenceId) 
+                                                    VALUES (@Id, @ApplicationId, @SequenceNo, @SectionNo, @QnaData, @Title, @LinkTitle, @DisplayType, @SequenceId)",
+                                                    new {Id = applySection.Id, ApplicationId = qnaApplicationId, SequenceNo = applySequence.SequenceId, SectionNo = applySection.SectionId, QnaData = applySection.QnAData, Title = applySection.Title, LinkTitle = applySection.LinkTitle, DisplayType = applySection.DisplayType, SequenceId = applySequence.Id});
+                        }
+                    }
                 }
             }
 
 
             // For each existing in-flight Application in Apply.
-            // Create Qna Applications record
+            
             // Convert ApplicationData
             // Create Qna ApplicationSequences record
             // Create Qna ApplicationSections record
+            // Modify QnAData to new format.
             // Create Assessor Apply record.
             // Create Organisation record if it ain't there.
             
