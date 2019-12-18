ba7d0244ac65180466e208bbeb87ea4bbbc0b012
Dave Gouge <davegouge@gmail.com>
2019-12-18 13:59:39 +0000

    Bumped version

--- src/SFA.DAS.Assessor.Functions/ApplicationsMigrator/ApplicationsMigrator.cs
+++ src/SFA.DAS.Assessor.Functions/ApplicationsMigrator/ApplicationsMigrator.cs
@@ -53,7 +53,7 @@ namespace SFA.DAS.Assessor.Functions.ApplicationsMigrator
                     var applyApplications = _dataAccess.GetCurrentApplyApplications(applyConnection);
 
                     totalApplicationsToMigrate = applyApplications.Count;
-                    log.LogTrace($"Version: RetainingGuids");
+                    log.LogTrace($"Version: Who ya gonna call?");
                     log.LogTrace($"Number of applications to Migrate: {totalApplicationsToMigrate}");
 
                     var applicationsProcessed = 0;
