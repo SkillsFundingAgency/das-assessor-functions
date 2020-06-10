c960b5e599296ab51002a5048521cfd463fe76c5
Kevin <kevin@stephek.co.uk>
2020-06-10 13:36:24 +0100

    CON-1834-template-changes

--- src/SFA.DAS.Assessor.Functions/Functions/Print/DeliveryNotificationFunction.cs
+++ src/SFA.DAS.Assessor.Functions/Functions/Print/DeliveryNotificationFunction.cs
@@ -16,7 +16,7 @@ namespace SFA.DAS.Assessor.Functions.Functions.Print
         }
 
         [FunctionName("DeliveryNotificationFunction")]
-        public async Task Run([TimerTrigger("%DeliveryNotificationFunctionFunctionSchedule%", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("%DeliveryNotificationFunctionSchedule%", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
         {
             try
             {
