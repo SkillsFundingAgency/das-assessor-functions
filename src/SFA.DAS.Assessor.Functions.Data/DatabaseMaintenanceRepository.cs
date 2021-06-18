070ff9b1bc916f4abf72adedb0ea47790d7eb490
Chris Woodcock <chris@humesoftware.com>
2021-06-18 10:09:46 +0100

    Increased timeout to 4 hours to allow long running database index rebuild to complete

--- src/SFA.DAS.Assessor.Functions.Data/DatabaseMaintenanceRepository.cs
+++ src/SFA.DAS.Assessor.Functions.Data/DatabaseMaintenanceRepository.cs
@@ -29,7 +29,7 @@ namespace SFA.DAS.Assessor.Functions.Data
         public async Task<List<string>> DatabaseMaintenance()
         {
             var results = await _connection.QueryAsync<string>("DatabaseMaintenance",
-                commandTimeout: 7200,
+                commandTimeout: 14400,
                 commandType: CommandType.StoredProcedure);
 
             return results
