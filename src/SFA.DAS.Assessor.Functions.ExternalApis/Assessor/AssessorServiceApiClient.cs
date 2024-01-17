1d82b38ea7cc9e594419c2bbee06aab323f0fea2
Jordan-fujitsu <jordan.singh@fujitsu.com>
2024-01-17 15:22:55 +0000

    reverting baseurl assessor client

--- src/SFA.DAS.Assessor.Functions.ExternalApis/Assessor/AssessorServiceApiClient.cs
+++ src/SFA.DAS.Assessor.Functions.ExternalApis/Assessor/AssessorServiceApiClient.cs
@@ -14,7 +14,7 @@ namespace SFA.DAS.Assessor.Functions.ExternalApis.Assessor
             HttpClient httpClient,
             IOptions<AssessorManagedIdentityClientConfiguration> options,
             ILogger<AssessorServiceApiClient> logger)
-            : base(httpClient, new Uri("https://at-assessors-api.apprenticeships.education.gov.uk"), logger)
+            : base(httpClient, new Uri(options?.Value.ApiBaseUrl), logger)
         {
         }
 
