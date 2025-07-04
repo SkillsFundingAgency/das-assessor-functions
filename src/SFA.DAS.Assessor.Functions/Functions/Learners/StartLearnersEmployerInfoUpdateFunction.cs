5a9e257d7b7042d1a2738d2f75ce839b3c3590e4
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-07-04 14:58:03 +0100

    P2-1769-Disabled unused function

--- src/SFA.DAS.Assessor.Functions/Functions/Learners/StartLearnersEmployerInfoUpdateFunction.cs
+++ src/SFA.DAS.Assessor.Functions/Functions/Learners/StartLearnersEmployerInfoUpdateFunction.cs
@@ -9,34 +9,34 @@ namespace SFA.DAS.Assessor.Functions.Functions.Learners
     public class StartLearnersEmployerInfoUpdateFunction
     {
         private readonly IEnqueueApprovalLearnerInfoBatchCommand _command;
-        private readonly ILogger<StartLearnersEmployerInfoUpdateFunction> _logger;   
+        private readonly ILogger<StartLearnersEmployerInfoUpdateFunction> _logger;
 
         public StartLearnersEmployerInfoUpdateFunction(IEnqueueApprovalLearnerInfoBatchCommand command, ILogger<StartLearnersEmployerInfoUpdateFunction> logger)
         {
             _command = command;
             _logger = logger;
         }
+        // This function has been commented as it's currently not in use.We may need it sometime soon for the digital certificates.
+        //[Function("StartLearnersEmployerInfoUpdateFunction")]
+        //public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+        //{
+        //    try
+        //    {
+        //        _logger.LogInformation($"StartLearnersEmployerInfoUpdateFunction has started.");
 
-        [Function("StartLearnersEmployerInfoUpdateFunction")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
-        {
-            try
-            {
-                _logger.LogInformation($"StartLearnersEmployerInfoUpdateFunction has started.");
+        //        await _command.Execute();
 
-                await _command.Execute();
+        //        _logger.LogInformation($"StartLearnersEmployerInfoUpdateFunction has finished.");
 
-                _logger.LogInformation($"StartLearnersEmployerInfoUpdateFunction has finished.");
+        //        await Task.CompletedTask;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, $"StartLearnersEmployerInfoUpdateFunction has failed.");
+        //        throw;
+        //    }
 
-                await Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"StartLearnersEmployerInfoUpdateFunction has failed.");
-                throw;
-            }
-
-            return new OkObjectResult("Start Learners EmployerInfo Update Function finished");
-        }
+        //    return new OkObjectResult("Start Learners EmployerInfo Update Function finished");
+        //}
     }
 }
